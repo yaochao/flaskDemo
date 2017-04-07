a34e94f76ad57d5a84535e3e4260e265bd01bc78
yaochao <yaochao365@qq.com>
2017-04-07 17:32:10 +0800

    demo api file

--- api.py
+++ api.py
@@ -1,52 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by yaochao on 2016/8/15
+from flask import Flask, request
+from flask_restful import Api, Resource, request
 import requests
-from flask import Flask
-from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-results = []
-
+token_ip138 = 'fd4711d74049c7a98dcaede7f4e17c94'
 
 class GetIP(Resource):
     def get(self):
-        return results, 200
+        # ip
+        remote_addr = request.remote_addr
+        address = self.ip138(remote_addr)
+        # ua
+        headers = request.headers.get('User-Agent')
+        user_agent = request.user_agent
+        print headers
 
+        return {
+            'ip': remote_addr,
+            'address': address,
+        }
 
-def get_ips():
-    url = 'http://api.zdaye.com/?api=201609221423569186&sleep=1%C3%EB%C4%DA&gb=2&https=%D6%A7%B3%D6&ct=50'
-    response = requests.get(url=url)
-    if response.status_code == 200:
-        if not response.content.startswith('<bad>'):
-            ips = response.content.split('\r\n')
-            return ips
-
-
-def check(proxy):
-    import urllib2
-    url = "https://www.baidu.com/js/bdsug.js"
-    proxy_handler = urllib2.ProxyHandler({'https': "https://" + proxy})
-    opener = urllib2.build_opener(proxy_handler, urllib2.HTTPHandler)
-    try:
-        response = opener.open(url, timeout=3)
-        return response.code == 200
-    except Exception:
-        return False
-
-
-def main():
-    ips = get_ips()
-    for ip in ips:
-        if check(ip):
-            results.append(ip)
-            print ip
-
+    def ip138(self, ip):
+        url = 'http://api.ip138.com/query/?datatype=jsonp&ip=' + str(ip)
+        response = requests.get(url, headers={'token': token_ip138})
+        if response.status_code == 200:
+            rst = response.json()
+            if rst['ret'] == u'ok':
+                return ''.join(rst['data'])
 
 api.add_resource(GetIP, '/ip')
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    main()
+    app.run(host='0.0.0.0', port=8888)
